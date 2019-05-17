88c37c1a4547cc3217dc15c92488be956c5638d7
Rachel R <rrrachelrath@gmail.com>
2019-05-16 22:06:43 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,35 @@
 class Employee
+  @@all =[]
 
+  attr_reader :name
+  attr_accessor :salary, :department, :manager_name
+
+  def initialize(name, salary)
+    @name = name
+    @salary = salary
+    @department = department
+    @manager_name = manager_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.paid_over(num)
+    @@all.select {|employee| employee.salary > num}
+  end
+
+  def self.find_by_department(department)
+    @@all.find {|employee| employee.department = department}
+    # return FIRST employee working there
+    # .find
+  end
+
+  def tax_bracket
+
+  end
 end
+#
+# Employee#tax_bracket
+# returns an Array of all the employees whose salaries are within $1000 (± 1000) of the employee who invoked the method
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,44 @@
 class Manager
+  @@all = []
+
+  attr_reader :name
+  attr_accessor :age, :department, :employees
+
+  def initialize(name, age, department)
+    @name = name
+    @age = age
+    @department = department
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def employees
+    Employee.all.select{|employee| employee.manager_name = self}
+  end
+
+  def hire_employee(name, salary)
+    employee = Employee.new(name, salary)
+     employee.department=(self.department)
+     employee.manager_name=(self.name)
+  end
+
+  def self.all_departments
+    # @@all.map {|manager| }
+  end
+
+  def self.average_age
+    # returns float
+  end
 
 end
+
+
+#
+
+# Manager.all_departments
+# returns an Array of all the department names that every manager oversees
+# Manager.average_age
+# returns a Float that is the average age of all the managers
